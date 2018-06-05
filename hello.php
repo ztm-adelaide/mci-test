697fc24ca019ceffbb8d4f9cdacc7ce4e0ab5251
ztm-adelaide <39851061+ztm-adelaide@users.noreply.github.com>
2018-06-06 00:43:10 +0930

    Update hello.php

--- hello.php
+++ hello.php
@@ -5,4 +5,4 @@
 <body> //https://sites.google.com/site/mci09test/ 
 <?php echo '<p>Hello World</p>'; ?>//https://sites.google.com/site/mcit09t01/ 
 </body>//https://sites.google.com/site/mci09test05/ 
-</html>
+</html> //https://sites.google.com/site/mci09test/ 
